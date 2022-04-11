1a62d659a6154d1a4e694750f81b201947e6a337
Audhil <smdaudhilbe@gmail.com>
2022-04-11 16:42:28 +0530

    add basic operations

---
+++ app.js
@@ -0,0 +1,11 @@
+const http = require('http')  //  require http module
+http.createServer(function(req, res) {
+  // res.write("Welcome back Man!")
+  // res.end()
+  //  above 2 lines are equal to below res.end("Welcome back Audhil!")
+  res.writeHead(200, {
+    'Content-Type': 'text/html'
+  })
+  res.end("Welcome back Audhil!")
+
+}).listen(8080)
---
+++ app2.js
@@ -0,0 +1,18 @@
+//  sample 1
+// function add(a, b) {
+//   return a+b
+// }
+// result = add(5,4)
+// console.log("the output is : "+result)
+
+//  sample 2
+var calc = require('./calc.js')
+result = calc.add(5,4)
+console.log("the add output is : "+result)
+result = calc.sub(5,4)
+console.log("the sub output is : "+result)
+
+result = calc.addd(5,4)
+console.log("the addd output is : "+result)
+result = calc.subb(5,4)
+console.log("the subb output is : "+result)
